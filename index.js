e47db45b0abef9a5c2bdc142fd5f4b95d4a8b8fd
freenih10 <freenih10@gmail.com>
2024-05-29 08:11:01 +0700

    thre

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // handler.js
-import { chromium } from 'playwright-chromium';
+const puppeteer = require('puppeteer');
+
+async function getLinks() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://vienze.com');
+
+    // Mengambil semua elemen <a> dari halaman web
+    const links = await page.evaluate(() => {
+        const anchorTags = Array.from(document.querySelectorAll('a'));
+        return anchorTags.map(tag => {
+            return {
+                href: tag.href,
+                text: tag.innerText
+            };
+        });
+    });
+
+    await browser.close();
+    return links;
+}
 
 export default async function handler(request, response) {
-    const url = 'https://vienze.com';
-
     try {
-        const browser = await chromium.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        // Mengambil semua elemen <a> dari halaman web
-        const links = await page.evaluate(() => {
-            const anchorTags = Array.from(document.querySelectorAll('a'));
-            return anchorTags.map(tag => {
-                return {
-                    href: tag.href,
-                    text: tag.innerText
-                };
-            });
-        });
-
-        await browser.close();
-
-        // Mengirim kembali hasil sebagai respons JSON
-        response.setHeader('Content-Type', 'application/json');
-        response.send(JSON.stringify(links));
+        const links = await getLinks();
+        response.status(200).json(links);
     } catch (error) {
         response.status(500).json({ error: error.message });
     }
